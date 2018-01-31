afe552ea565ba1122f55eb280863378be738c111
Softcoregamerz <softcoregamerz@hotmail.com>
2018-01-31 18:17:47 +0100

    Sehr gute version
    1 bug noch

--- script.js
+++ script.js
@@ -16,21 +16,24 @@ var memory_array = ['mario_coin.jpg','mario_coin.jpg','pubg_helmet.jpg','pubg_he
 var memory_values = [];
 //
 var memory_tile_ids = [];
-// Dies ist eine variable die dazu dient den Status des Spiels zu definieren. 
-// 0 bedeutet das gerade keine Karte umgedreht wurde
-// 1 bedeutet das gerade eine Karte umgedreht wurde
-// 2 bedeutet das 2 Karten gerade umgedreht wurden
+// Dies ist eine variable die dazu dient sich zu merken wie viele Tiles umgedreht wurden. Am ende wird dieser wert mit der anzahl der tiles verglichen
 var tiles_flipped = 0;
 
 // dies ist eine Funktion die der Java Klasse 'Array' hinzugefügt wird da ihnen gewissen funktionalitäten fehlen
 // diese Funktion mischt die Tiles neu in einer zufälligen Anordung
-Array.prototype.memory_tile_shuffle = function(){
+// Sie basiert auf dem Fisher-Yaters Modern Shuffle Algorithm
+Array.prototype.memory_tile_shuffle = function()
+{
+// 3 Variablen werden hier definiert. i ist die Länge der Wert der Länge des Arrays, j wird zur speicherung einer zufälligen Zahl benutzt und temp ist dazu da 1 bestimmtes element im array mit einem anderen zu tauschen
     var i = this.length, j, temp;
+// der Wert i(länge des arrays) wird jedes mal -1 gerechnet bis es zu 0 kommt
     while(--i > 0){
+// Hier wird eine zufällige Zahl zwischen 0 und dem Array.length-1 generiert 
         j = Math.floor(Math.random() * (i+1));
         temp = this[j];
         this[j] = this[i];
         this[i] = temp;
+// Und am ende wird das object an der position index mit dem object an der zufälligen position "getauscht"
     }
 }
 // Diese Funktion generiert ein neues Feld aufdem die Tiles gelegt werden, dies ist sozusagen die Play() Funktion wodurch das Spiel anfängt
@@ -39,52 +42,66 @@ function newBoard()
 {
  tiles_flipped = 0;
  var output = '';
+// hier wird das array genommen und alle tiles werden vermischt
  memory_array.memory_tile_shuffle();
     
 // Dies ist eine For-Schleife, sie wiederholt den in ihr liegenden Code so oft wie das Array: memory_array lang ist. 
  for(var i = 0; i < memory_array.length; i++)
  {
 // Hier wird jedem Element in dem Array ein div gegeben und einem id mit unterschiedlichen Nummer basierend von 0 bis zur Länge des Arrays
-// dann bekommt jedes Tile eine Funktion hinzugefügt mit den jeweiligen gebrauchten Parametern und zwar: (tile,val) was durch this
+// dann bekommt jedes Tile eine Funktion hinzugefügt mit den jeweiligen gebrauchten Parametern und zwar: (tile,val) was durch this und memory_array definiert ist
   output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
  }
+// hier werden alle divs in das memory board gemachts
  document.getElementById('memory_board').innerHTML = output;
 }
 
-
+// In dieser Funktion werden 2 Werte übergeben, 
+// tile ist 
 function memoryFlipTile(tile,val)
 {
+//
  if(tile.innerHTML == "" && memory_values.length < 2)
  {
     tile.style.background = '#FFF';
+// Hier wird der Wert von Value was alles in der jeweilign position des arrays ist, in das jeweilige Tile gemacht und dazu <img src> rangehangen. Dadurch hat dann das jweilige Tile ein Bild
     tile.innerHTML = '<img src="' + val + '">';
+// Hier wird gecheckt ob irgendwas schon in memory_value ist. 
+// Falls nichts drinnen ist wird der code ausgeführt welcher sich die Karte merkt
         if(memory_values.length == 0)
         {
-            if(tiles_flipped == memory_array.length - 1)
-                {
-                alert("Board cleared... generating new board");
-                    document.getElementById('memory_board').innerHTML = "";
-                    newBoard();
-                }
+            // Hier wird der ganze Inhalt in ein neues Array gemacht
             memory_values.push(val);
+            // Und hier wird die ID des Tiles in das Array gemacht
             memory_tile_ids.push(tile.id);
         }
+// Falls aber schon ein wert in dem array ist, also eine andere Karte geklickt wurde wird folgender code ausgeführt
  else if(memory_values.length == 1)
  {
+// diese Karte wird auch dem array hinzugefüg
    memory_values.push(val);
    memory_tile_ids.push(tile.id);
+// und hier werden beide objecte im array verglichen und geguckt ob sie gleich sind
    if(memory_values[0] == memory_values[1])
    {
     tiles_flipped += 2;
-    // Clear both arrays
+    // beide arrays werden gelöscht damit neue karte gecheckt werden können
     memory_values = [];
-             memory_tile_ids = []; 
-   } 
-     
+    memory_tile_ids = []; 
+   
+// hier wird die anzahl der umgedrehten tiles mit der gesamten anzahl aller tiles verglichen
+    if(tiles_flipped == memory_array.length )
+                {
+                alert("Glückwunsch! Du hast das Memory geschaft. Ein neues zufälliges Memory wird generiert ");
+                    document.getElementById('memory_board').innerHTML = "";
+                    newBoard();
+                }
+   }
+// Falls beide tiles in dem Array nicht die gleichen sind wird folgender code ausgeführt 
  else {
      
 // Hier werden beide Tiles wieder zurück gedreht
-    function flip2Back()
+    function flipBack()
      {
         var tile_1 = document.getElementById(memory_tile_ids[0]);
         var tile_2 = document.getElementById(memory_tile_ids[1]);
@@ -94,15 +111,13 @@ function memoryFlipTile(tile,val)
         tile_1.innerHTML = "";
         tile_2.style.background = 'url(memory_karten_rücken.jpg) no-repeat';
         tile_2.style.backgroundSize = '110px 110px';
-
         tile_2.innerHTML = "";
          
-        // Clear both arrays
         memory_values = [];
-                 memory_tile_ids = [];
+        memory_tile_ids = [];
     }
-     // Funktion flip2Back zuende 
-    setTimeout(flip2Back, 700);
+     // Funktion flipBack zuende 
+    setTimeout(flipBack, 1000);
    }
     // Letztes Else der function memoryFlipTile() funktin
 
